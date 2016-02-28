cef413187b771f4f8710b0d1da45db135cbba3f1
Lemon113 <oparisheg113@gmail.com>
2016-02-28 18:47:49 +1000

    update
    market.sql содержит дамп БД с указанием связей

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Best internet market ever</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<h1 class="topic">There are the best market you ever seen</h1>
+	<div id="content">
+		<div id="menu">
+			<ul>
+				<li><a href="addGoods.php">ДОБАВИТЬ ТОВАР</a></li>
+				<li><a href="addMarks.php">ДОБАВИТЬ МАРКИ</a></li>
+				<li><a href="#">ДОБАВИТЬ ХАРАКТЕРИСТИКИ</a></li>
+				<li><a href="#">ДОБАВИТЬ ТИПЫ</a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
